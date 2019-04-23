de8c1e5de8dd6bdfa18b27f8fc8fe1029e7ae8f0
srhbitbucket <henneberrysteven@gmail.com>
2019-04-22 20:09:29 -0700

    -simplified index.js

--- server/index.js
+++ server/index.js
@@ -1,51 +1,80 @@
-var express = require('express'); 
-var server = require('http').createServer(handler).listen(4000, function(){
-  console.log('listening to request on port 4000')
+// var fs = require('fs');
+// var express = require('express'); 
+// var socket = require('socket.io');
+
+// //App setup
+// var app = express();
+// var server = app.listen(4000, function(){
+//   console.log('listening to request on nodemon');
+// })
+
+// //Static files. Most static files come from the server side
+// app.use(express.static('client'));//searches for index.html file in client folder
+// //sets up socket on server side
+// var io = socket(server);
+
+
+
+
+// //client's starts io connection, a new socket is made, and
+// // the callback function defines what to do w/newly created socket
+// io.on('connection', function (socket/*bidirectional socket*/) {
+//   //This emits to either the client or the server depending 
+//   //on which side this script is being run from
+//   //In this case, this is being emitted to the client
+//   console.log('received connection');
+//   //test connection
+//   socket.emit('news', { hello: 'world' });
+//   socket.on('post', function(data){
+//     //Save data to MongoDB
+//     console.log('received post'); 
+//   }); 
+//   socket.on('my other event', function (data) {
+//       //listens and log data from client
+//     console.log(data);
+//   });
+// });
+var path = require('path');
+var express = require('express');
+var app = express();//creates an instance of the express object
+//declared above. 
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(4000);
+
+var options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html'],
+  index: false,
+  maxAge: '1d',
+  redirect: false,
+  setHeaders: function (res, path, stat) {
+    res.set('x-timestamp', Date.now())
+  }
+}
+
+//sets up a static file directory to refer to. 
+app.use(express.static(path.join(__dirname, '..', 'client')));
+
+// app.get('/', function (req, res) {
+//   const indexPath = path.join(__dirname, '..', 'client', 'index.html' );
+//   res.sendFile(indexPath);
+// });
+
+//Use express for things that don't have to be updated live
+app.post('/SubmitVermin'/*Refers to form action in html*/, function(req, res){
+   
 });
 
-//sets socket io to work on the server specified
-var socket = require('socket.io');
-//sets up socket on server side
-var io = socket(server); 
-
-var fs = require('fs');
-
-
-//App setup
-var app = express();
-
-
-
-
-function handler (req, res) {
-  fs.readFile(__dirname + '/../client/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);//this is what is sent back to the client
-    //if everything goes ok
-  });
-}
-console.log(Object.keys(io))
-//client's starts io connection, a new socket is made, and
-// the callback function defines what to do w/newly created socket
-io.on('connection', function (socket/*bidirectional socket*/) {
-  //This emits to either the client or the server depending 
-  //on which side this script is being run from
-  //In this case, this is being emitted to the client
-  console.log('received request');
-  //test connection
+//only use socket for real time updates
+io.on('connection', function (socket) {
+  console.log('connection set')
   socket.emit('news', { hello: 'world' });
-  socket.on('post', function(data){
-    //Save data to MongoDB
-    console.log('received post'); 
-  }); 
   socket.on('my other event', function (data) {
-      //listens and log data from client
+    console.log('received post request');
     console.log(data);
   });
 });
+
